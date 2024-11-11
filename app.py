2550f30933b8d7e3c539c39019ee1674c21b470c
xuanle22 <49552696+xuanle22@users.noreply.github.com>
2024-11-11 16:04:56 +0000

    update

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 from flask import Flask
+from flask import render_template, request
+
+app = Flask("__name__") ### handphone, app和文件名重，和colab有区别？？
+
+
+@app.route("/",methods =["GET","POST"]) #/是staring point,
+def index():
+    return(render_template("index.html"))
+
+if __name__ == "__main__": #magic name, double confirm, responsable
+    app.run() 
+
+
+
