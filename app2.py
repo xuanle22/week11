ac174eb074414e3c50c6c5259e6a0da7aa85dbd4
xuanle22 <49552696+xuanle22@users.noreply.github.com>
2024-11-14 09:25:21 +0000

    try

--- app2.py
+++ app2.py
@@ -1,12 +1,12 @@
 from flask import Flask
-app = Flask(__name__)
+app3 = Flask(__name__) #app3
 from flask import request, render_template
-@app.route("/", methods = ["GET", "POST"])
+@app3.route("/", methods = ["GET", "POST"]) #app3
 def i():
-    if request.method == "POST":
-        num = float(request.form.get("rates"))
-        return(render_template("index.html", result = 90.2-(50.6*num)))
+    if request.method == "POST": #post?
+        num = float(request.form.get("rates"))  #rates: html里的name
+        return(render_template("index2.html", result = 90.2-(50.6*num))) #result: html里的变量
     else:
-        return render_template("index.html", result ="Waiting……….")
+        return render_template("index2.html", result ="Waiting……….")
 if __name__=="__main__":
-    app.run()
+    app3.run() #app3
